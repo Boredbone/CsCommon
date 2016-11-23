0e18178b173b9c9bda84d8fd2c82aa47741fb7c0
boredbone <Boredbone@users.noreply.github.com>
2016-11-23 17:49:00 +0900

    LockingCollection

---
+++ Utility/LockingCollection/LockingList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boredbone.Utility.LockingCollection
+{
+    public class LockingList<T>
+    {
+        protected object gate = new object();
+
+        protected List<T> list;
+
+        public LockingList()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            lock (this.gate)
+            {
+                this.list.Add(item);
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            lock (this.gate)
+            {
+                foreach (var item in this.list)
+                {
+                    action(item);
+                }
+            }
+        }
+
+        public T[] ToArray()
+        {
+            lock (this.gate)
+            {
+                return this.list.ToArray();
+            }
+        }
+
+        public void Clear()
+        {
+            lock (this.gate)
+            {
+                this.list.Clear();
+            }
+        }
+    }
+}
